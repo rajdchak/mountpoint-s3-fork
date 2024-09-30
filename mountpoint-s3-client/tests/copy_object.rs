253c5690f5d9a056b0b58157371b1ad418d4ae08
rajdchak <rajdchak@amazon.co.uk>
2024-09-30 23:34:44 +0100

    Added better logging

--- mountpoint-s3-client/tests/copy_object.rs
+++ mountpoint-s3-client/tests/copy_object.rs
@@ -1,7 +1,7 @@
 #![cfg(feature = "s3_tests")]
 
 pub mod common;
-
+use tracing::info;
 use common::*;
 use mountpoint_s3_client::error::{ObjectClientError};
 use mountpoint_s3_client::{ObjectClient, S3CrtClient};
@@ -9,13 +9,13 @@ use mountpoint_s3_client::{ObjectClient, S3CrtClient};
 #[tokio::test]
 async fn test_copy_objects() {
     let sdk_client = get_test_sdk_client().await;
-    let (bucket, prefix1) = ("copy-rajdchak1", "prefix1/");
+    let (bucket, prefix1) = ("\
+    ", "prefix1/");
 
     let client: S3CrtClient = get_test_client();
 
-    let result = client
-        .copy_object(&bucket, &prefix1, &bucket, "prefix2/")
-        .await;
-
-    println!("rajdchak = {result:?}");
+    match client.copy_object(&bucket, &prefix1, &bucket, "prefix2/").await {
+        Ok(result) => info!("Copy operation successful: {:?}", result),
+        Err(e) => error!("Error during copy operation: {:?}", e),
+    }
 }
