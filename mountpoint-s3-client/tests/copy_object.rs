0ba883c0cab799b726a5821d42f85f7c574122c2
rajdchak <rajdchak@amazon.co.uk>
2024-10-04 10:51:51 +0100

    Made modifications to some tests

--- mountpoint-s3-client/tests/copy_object.rs
+++ mountpoint-s3-client/tests/copy_object.rs
@@ -8,20 +8,6 @@ use mountpoint_s3_client::error::{ObjectClientError};
 use mountpoint_s3_client::{ObjectClient, S3CrtClient};
 use aws_sdk_s3::primitives::ByteStream;
 use bytes::Bytes;
-
-#[tokio::test]
-async fn test_copy_objects_old() {
-    let sdk_client = get_test_sdk_client().await;
-    let (bucket, prefix1) = ("copy-rajdchak1", "prefix1/largefile");
-
-    let client: S3CrtClient = get_test_client();
-
-    match client.copy_object(&bucket, &prefix1, &bucket, "prefix1/largefile2020").await {
-        Ok(result) => info!("Copy operation successful: {:?}", result),
-        Err(e) => error!("Error during copy operation: {:?}", e),
-    }
-}
-
 #[tokio::test]
 async fn test_copy_objects() {
     let sdk_client = get_test_sdk_client().await;
@@ -45,7 +31,7 @@ async fn test_copy_objects() {
     let _result = client
         .copy_object(&bucket, &key, &bucket, &copy_key)
         .await
-        .expect("copy_object should succeed");
+        .expect("copy_object operation should succeed");
 
     let head_obj = sdk_client
         .head_object()
@@ -53,5 +39,23 @@ async fn test_copy_objects() {
         .key(&copy_key)
         .send()
         .await
-        .expect("object should exist");
+        .expect("copied object should exist");
 }
+#[tokio::test]
+async fn test_copy_object_no_permission() {
+    let (_bucket, prefix) = get_test_bucket_and_prefix("test_copy_object_no_permission");
+    let bucket = get_test_bucket_without_permissions();
+    let key = format!("{prefix}/hello");
+    let copy_key = format!("{prefix}/hello2");
+
+    let client: S3CrtClient = get_test_client();
+
+    let result = client.copy_object(&bucket, &key, &bucket, &copy_key).await;
+
+    assert!(matches!(
+        result,
+        Err(ObjectClientError::ClientError(S3RequestError::Forbidden(_, _)))
+    ));
+}
+
+// TODO: Add integration test for cross bucket copy but before that need to set up a new environment variable for a new bucket.
